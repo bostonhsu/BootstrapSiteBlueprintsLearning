5e3562e7a77304d42121d940cec0c817f6547c7d
徐智勇 <bostonhsu@outlook.com>
2018-12-21 10:06:52 +0800

    just format codes.

--- html5-boilerplate-0.9.5/js/plugins.js
+++ html5-boilerplate-0.9.5/js/plugins.js
@@ -1,14 +1,14 @@
 
 // remap jQuery to $
-(function($){
-
- 
+(function ($) {
 
 
 
 
 
- 
+
+
+
 
 
 
@@ -18,22 +18,22 @@
 
 // usage: log('inside coolFunc',this,arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function(){
+window.log = function () {
   log.history = log.history || [];   // store logs to an array for reference
   log.history.push(arguments);
-  if(this.console){
-    console.log( Array.prototype.slice.call(arguments) );
+  if (this.console) {
+    console.log(Array.prototype.slice.call(arguments));
   }
 };
 
 
 
 // catch all document.write() calls
-(function(doc){
+(function (doc) {
   var write = doc.write;
-  doc.write = function(q){ 
-    log('document.write(): ',arguments); 
-    if (/docwriteregexwhitelist/.test(q)) write.apply(doc,arguments);  
+  doc.write = function (q) {
+    log('document.write(): ', arguments);
+    if (/docwriteregexwhitelist/.test(q)) write.apply(doc, arguments);
   };
 })(document);
 
